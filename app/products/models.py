1fc62584a382be627ae58479977df1fc363683bb
Sbtah <shisastore@protonmail.com>
2022-08-22 17:07:51 +0200

    Added Product model.

--- app/products/models.py
+++ app/products/models.py
@@ -13,12 +13,29 @@ class Product(models.Model):
     product_brand = models.CharField(max_length=255, blank=True)
     product_sku = models.CharField(max_length=255, blank=True, unique=True)
     product_description = models.TextField()
-    product_traits = ArrayField(models.CharField(max_length=100, blank=True))
+    product_traits = ArrayField(
+        models.CharField(max_length=100, blank=True),
+        blank=True,
+        null=True,
+    )
     product_promo_type = models.CharField(max_length=255, blank=True)
     product_price_for_unit = models.DecimalField(
         max_digits=6, decimal_places=2, blank=True, null=True
     )
-    product_unit_type = models.CharField(max_length=20, blank=True)
-    roduct_price_for_piece = models.DecimalField(
+    product_unit_type = models.CharField(max_length=50, blank=True)
+    product_price_for_piece = models.DecimalField(
         max_digits=6, decimal_places=2, blank=True, null=True
     )
+    product_piece_type = models.CharField(max_length=50, blank=True)
+    product_price_before_promo = models.DecimalField(
+        max_digits=6, decimal_places=2, blank=True, null=True
+    )
+    product_warranty_time = models.CharField(max_length=100, blank=True)
+    product_ean = models.CharField(max_length=13, blank=True)
+    product_availability = models.CharField(max_length=50, blank=True)
+    product_current_stock = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
+
+    def __str__(self):
+        return f"{self.name}"
