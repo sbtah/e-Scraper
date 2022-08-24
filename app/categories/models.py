9b1d734ae8e61b0040e1fcb9d88e7e1572bb52a5
Sbtah <shisastore@protonmail.com>
2022-08-24 13:24:24 +0200

    Added Category model

--- app/categories/models.py
+++ app/categories/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Class for category object."""
+
+    current_url = models.CharField(max_length=255, unique=True)
+    category_name = models.CharField(max_length=255)
+
+    is_active = models.BooleanField()
+    category_description = models.TextField(blank=True)
+    meta_description = models.TextField(blank=True)
+    canonical_url = models.CharField(max_length=255, blank=True)
+    seo_title = models.CharField(max_length=255, blank=True)
+    parrent_category = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name_plural = "Categories"
