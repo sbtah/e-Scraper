50c9f45a381083bf1cc8ee98db259e7ff108d425
Sbtah <shisastore@protonmail.com>
2022-09-08 06:28:04 +0200

    Modified Category builder, to track last discovery date for object.

--- app/categories/builders.py
+++ app/categories/builders.py
@@ -1,35 +1,42 @@
 """
 Helper functions for creating or updating new Category objects.
 """
+from datetime import datetime
 from categories.models import Category
 from scraper.helpers.logging import logging
 
 
 # TODO:
-# Add some persistand logging mechanism in second DB.
+# Add some persistant logging mechanism in second DB.
 
 
 def update_or_create_category(
-    current_url,
+    discovery_url,
     category_name,
 ):
-    """Create or Update an instance of Category object."""
-
+    """
+    Create or Update an instance of Category object.
+    Used at discovery level, where only Names and Urls are created.
+    """
+    now = datetime.strptime(
+        datetime.now().strftime("%d/%m/%Y, %H:%M:%S"), "%d/%m/%Y, %H:%M:%S"
+    )
     try:
-        category = Category.objects.get(current_url=current_url)
-
+        category = Category.objects.get(discovery_url=discovery_url)
         if str(category_name) != str(category.category_name):
-            category.current_url = current_url
             category.category_name = category_name
+            category.last_discovery = now
             category.save()
-            logging.info(f"!! UPDATE Category: {category.category_name}")
+            logging.info(f"Found Category, updating name: {category.category_name}")
         else:
-            logging.info(f"NO UPDATES for Category: {category.category_name}")
+            category.last_discovery = now
+            category.save()
+            logging.info(f"Found Category: {category.category_name}")
     except Category.DoesNotExist:
         category = Category.objects.create(
-            current_url=current_url,
+            discovery_url=discovery_url,
             category_name=category_name,
+            last_discovery=now,
         )
-        logging.info(f"!! CREATE Category: {category.category_name}")
-
+        logging.info(f"!! CREATED New Category: {category.category_name}")
     return category
