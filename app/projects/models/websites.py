d5bdd25a1d1a3b388ed8a2f17bf4edd1f6519bcf
Sbtah <shisastore@protonmail.com>
2022-11-28 21:40:49 +0100

    Modified Website object

--- app/projects/models/websites.py
+++ app/projects/models/websites.py
@@ -4,9 +4,10 @@ from django.db import models
 class Website(models.Model):
     """Base class for Website object."""
 
+    domain = models.CharField(max_length=200, unique=True)
     main_url = models.CharField(max_length=200, unique=True)
-    module_name = models.CharField(max_length=100, unique=True)
-    scraper_class = models.CharField(max_length=100, blank=True)
+    module_name = models.CharField(max_length=100)
+    scraper_class = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.domain
