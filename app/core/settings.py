b32585980aaa670aa6308a7492809be600a6ae97
Sbtah <shisastore@protonmail.com>
2022-11-28 21:07:30 +0100

    Register models

--- app/core/settings.py
+++ app/core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = "django-insecure-+4d(i9675ec_gdr7u$%ke!5sljgtsrlc%lob7(i-*72zw)5ji$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- app/projects/admin.py
+++ app/projects/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from projects.models.about import AboutPage
+from projects.models.blogs import BlogPage
+from projects.models.categories import CategoryPage
+from projects.models.contact import ContactPage
+from projects.models.home import HomePage
+from projects.models.products import ProductPage
+from projects.models.websites import Website
 
-# Register your models here.
+
+admin.site.register(AboutPage)
+admin.site.register(BlogPage)
+admin.site.register(CategoryPage)
+admin.site.register(ContactPage)
+admin.site.register(HomePage)
+admin.site.register(ProductPage)
+admin.site.register(Website)
