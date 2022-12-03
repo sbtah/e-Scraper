a3677274b32f3f48698d72702ab96ebb863211d7
Sbtah <shisastore@protonmail.com>
2022-12-03 12:27:32 +0100

    Refactored BaseScaper. Removed unused async code since scaper relies fully on Selenium.

--- app/scraper/logic/base.py
+++ app/scraper/logic/base.py
@@ -1,23 +1,9 @@
-import asyncio
-import logging
-import sys
 from datetime import datetime
-from logging import Formatter, StreamHandler
 from urllib.parse import urljoin
 
-import httpx
-from lxml.html import (
-    HtmlElement,
-    HTMLParser,
-    document_fromstring,
-    fromstring,
-    tostring,
-)
-from scraper.helpers.randoms import (
-    random_sleep_medium,
-    random_sleep_small,
-    random_sleep_small_l2,
-)
+from lxml.html import HTMLParser, document_fromstring, fromstring
+from scraper.helpers.logger import logger
+from scraper.helpers.randoms import random_sleep_small, random_sleep_small_l2
 from selenium import webdriver
 from selenium.common.exceptions import (
     ElementNotVisibleException,
@@ -25,27 +11,21 @@ from selenium.common.exceptions import (
 )
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 class BaseScraper:
-    """Base scraper class that is working as a blueprint for other specialized scrapers."""
+    """Base scraper class for other specialized scrapers."""
 
     def __init__(self, *args, **kwargs):
         self._driver = None
         self.teardown = True
-        self.logger = logging.getLogger(__file__)
-        self.logger.setLevel(logging.INFO)
-        self.handler = StreamHandler(stream=sys.stdout)
-        self.handler.setFormatter(
-            Formatter(fmt="[%(asctime)s: %(levelname)s] %(message)s")
-        )
-        self.logger.addHandler(self.handler)
+        self.logger = logger
         self.time_started = datetime.now()
-        self.logger.info(f"Started scraper for : '{__file__}'")
-        # self.get_with_selenium = get_with_selenium
+        self.logger.info(f"Started scraper for : '{self.main_url}'")
 
     def __str__(self):
         return "Base Scraper"
@@ -54,7 +34,6 @@ class BaseScraper:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        #  and self.get_with_selenium
         if self.teardown:
             self.driver.delete_all_cookies()
             self.driver.quit()
@@ -67,16 +46,11 @@ class BaseScraper:
 
     @property
     def user_agent(self):
-        return "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+        return "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"  # noqa
 
     @property
     def driver(self):
 
-        # assert (
-        #     self.get_with_selenium == True
-        # ), "To start a driver you need to set get_with_selenium to True."
-
-        #  and self.get_with_selenium == True
         if self._driver is None:
             options = webdriver.ChromeOptions()
             options.add_argument("--width=1920")
@@ -94,13 +68,18 @@ class BaseScraper:
             # self.options.add_argument('--proxy-server=176.9.220.108:8080')
             # options.add_argument("--headless")
             options.add_argument("--disable-blink-features")
-            options.add_argument("--disable-blink-features=AutomationControlled")
+            options.add_argument(
+                "--disable-blink-features=AutomationControlled"
+            )  # noqa
             options.add_argument("--disable-infobars")
             options.add_argument("--ignore-ssl-errors=yes")
             options.add_argument("--ignore-certificate-errors")
             # Deprecated ?
             # options.add_experimental_option("useAutomationExtension", False)
-            options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            options.add_experimental_option(
+                "excludeSwitches",
+                ["enable-automation"],
+            )
             options.add_argument("--start-maximized")
 
             # Locally installed browser just for testing.
@@ -137,69 +116,9 @@ class BaseScraper:
             self.logger.error(f"(selenium_get) Exception: {e}")
             return None
 
-    def python_get(self, url):
-        """
-        Request specifed url.
-        :param url: Requested URL.
-        Return's text response.
-        """
-
-        # TODO:
-        # User-Agent Rotation
-        headers = {
-            "User-Agent": self.user_agent,
-        }
-
-        try:
-            response = httpx.get(url, timeout=30, headers=headers)
-            self.logger.info(f"Requesting with python: {url}")
-            random_sleep_small()
-            # response.raise_for_status()
-            return response.text
-        except httpx.TimeoutException:
-            self.logger.error("Connection was timed out.")
-            return None
-        except httpx.ConnectError:
-            self.logger.error("Connection Error.")
-            return None
-        except httpx.HTTPError:
-            self.logger.error("HTTPError was raised.")
-            return None
-        except Exception as e:
-            self.logger.error(f"(python_get) Exception: {e}")
-
-    async def async_get(self, session, url):
-        """
-        Request url with async.
-        :param url: Requested URL.
-        :param session: Session is used while connecting.
-        Return's text response.
-        """
-        response = await session.get(url)
-        # print(response.text)
-        return response.text
-
-    async def get_pages(self, session, urls_list):
-        """ """
-        tasks = []
-        for url in urls_list:
-            tasks.append(asyncio.create_task(self.async_get(session, url)))
-
-        requested = await asyncio.gather(*tasks)
-        return requested
-
-    async def execute_async_get(self, urls_list):
-        """ """
-        async with httpx.AsyncClient() as session:
-            data = await self.get_pages(session=session, urls_list=urls_list)
-            return data
-
-    def run_async(self, function):
-        asyncio.run(function)
-
     def parse_response(self, response):
         """
-        Parse text response from both python_get and selenium_get.
+        Parse text response from selenium_get.
         Returns HtmlElement.
         :param response: Text response from GET.
         """
@@ -258,8 +177,9 @@ class BaseScraper:
         Used with Selenium driver.
         Finds element by specified Xpath.
         Return Selenium element to interact with.
-        :param ignore_not_found_errors: Can be set to True to not produce error logs,
-            when element is not found.
+        :param ignore_not_found_errors:
+            - Can be set to True to not produce error logs,
+                when element is not found.
         """
         try:
             element = self.driver.find_element(
@@ -276,7 +196,7 @@ class BaseScraper:
                 return None
             else:
                 self.logger.error(
-                    f"(find_selenium_element) Selenium element not found. Is the Xpath ok?"
+                    f"(find_selenium_element) Selenium element not found. Is the Xpath ok?"  # noqa
                 )
                 return None
         except Exception as e:
@@ -292,8 +212,9 @@ class BaseScraper:
         Used with Selenium driver.
         Finds elements by specified Xpath.
         Return Selenium elements to interact with.
-        :param ignore_not_found_errors: Can be set to True to not produce error logs,
-            when elements are not found.
+        :param ignore_not_found_errors:
+            - Can be set to True to not produce error logs,
+                when elements are not found.
         """
         try:
             elements = self.driver.find_elements(
@@ -301,7 +222,7 @@ class BaseScraper:
                 xpath_to_search,
             )
             self.logger.info(
-                f"(find_selenium_elements), returned: {len(elements)} elements."
+                f"(find_selenium_elements), returned: {len(elements)} elements."  # noqa
             )
             return elements
         except ElementNotVisibleException:
@@ -324,24 +245,27 @@ class BaseScraper:
         ignore_not_found_errors=False,
     ):
         """
-        Xpath have to lead to entire HTML tag not attributes
         Finds elements by Xpath on given HTMLElement.
         Returns lists of HtmlElements for further processing.
-        :param ignore_not_found_errors: Can be set to True to not produce error logs,
-        when elements are not found.
+        Xpath have to lead to entire HTML tag not attributes.
+        :param ignore_not_found_errors:
+            - Can be set to True to not produce error logs,
+                when elements are not found.
         """
         try:
             elements_list = html_element.xpath(xpath_to_search)
             if elements_list:
                 self.logger.debug(
-                    f"(find_all_elements), returned: {len(elements_list)} elements."
+                    f"(find_all_elements), returned: {len(elements_list)} elements."  # noqa
                 )
                 return elements_list
             else:
                 if ignore_not_found_errors:
                     return None
                 else:
-                    self.logger.error("(find_all_elements) Returned an empty list.")
+                    self.logger.error(
+                        "(find_all_elements) Returned an empty list.",
+                    )
                     return None
         except Exception as e:
             self.logger.error(f"(find_all_elements) Some other exception: {e}")
@@ -352,16 +276,19 @@ class BaseScraper:
         Used with Selenium driver.
         Initialize a part of content that is loaded on click.
         Returns HTMLElement on success that can be parsed if other methods.
-        ::param xpath_to_click:: Xpath to element that we have to click to
-                                load desired element (ie: modal?).
+        ::param xpath_to_click::
+            - Xpath to element that we have to click to
+                load desired element (ie: modal?).
         """
         try:
+            # Yes I know that Selenium click() relies on move_to_element()
+            self.scroll_to_element(selenium_element=selenium_element)
             selenium_element.click()
             self.logger.debug("Successfully clicked on specified element.")
             random_sleep_small_l2()
         except NoSuchElementException:
             self.logger.error(
-                "Failed at finding element to click. Maybe element was already clicked?"
+                "Failed at finding element to click. Maybe element was already clicked?"  # noqa
             )
             return None
 
@@ -375,17 +302,22 @@ class BaseScraper:
             return True
         except IndexError:
             self.logger.debug(
-                f"(if_xpath_in_element) Search for: ('{xpath_to_search}') returned an empty list."
+                f"(if_xpath_in_element) Search for: ('{xpath_to_search}') returned an empty list."  # noqa
             )
             return None
         except Exception as e:
             self.logger.error(f"(if_xpath_in_element), Exception: {e}")
             return None
 
-    def extract_urls_with_names(self, html_element, xpath_to_search, name_attr_xpath):
+    def extract_urls_with_names(
+        self,
+        html_element,
+        xpath_to_search,
+        name_attr_xpath,
+    ):
         """
         Used for traversing page's url structure.
-        Find all Urls and 'Names' in given HtmlElements.
+        Finds all Urls and 'Names' in given HtmlElements.
         Returns generator of tuples, containing (url, name).
         :param xpath_to_search: Xpath that should return list of <a> tags.
         :param name_xpath: Can be set to ./text() or @title,
@@ -398,7 +330,7 @@ class BaseScraper:
 
         if categories_list:
             self.logger.info(
-                f"URLS/Names list created, returned {len(categories_list)} elements."
+                f"URLS/Names list created, returned {len(categories_list)} elements."  # noqa
             )
             return (
                 (
@@ -414,7 +346,7 @@ class BaseScraper:
     def extract_urls_with_names_selenium(self, xpath_to_search, url_name_attr):
         """
         Used for traversing page's url structure.
-        Find all main Urls and 'Names' in given HtmlElements.
+        Finds all main Urls and 'Names' in given HtmlElements.
         Returns generator of tuples, containing (url, name).
         :param xpath_to_search: Xpath that should return list of <a> tags.
         :param url_name_attr: Can be set to 'text' or 'title',
@@ -441,16 +373,36 @@ class BaseScraper:
 
     def send_text_to_element(self, text, selenium_element):
         """
-        Locate an imput field by Xpath and sends value to it.
+        Takes Selenium Element as an input, sends specified text to it.
         """
 
         try:
+            self.scroll_to_element(selenium_element=selenium_element)
             selenium_element.clear()
             selenium_element.send_keys(text)
             selenium_element.send_keys(Keys.ENTER)
             random_sleep_small()
-            self.logger.info(f"Successfuly send text: '{text}' to desired element.")
+            self.logger.info(
+                f"Successfuly send text: '{text}' to desired element.",
+            )
         except ElementNotVisibleException:
             self.logger.error("Specified element is not visible.")
         except Exception as e:
-            self.logger.error(f"(send_text_to_element) Some other exception: {e}")
+            self.logger.error(
+                f"(send_text_to_element) Some other exception: {e}",
+            )
+
+    def scroll_to_element(self, selenium_element):
+        """
+        Takes Selenium Element as an input. Scrolls to this element.
+        """
+        actions = ActionChains(self._driver)
+        try:
+            actions.move_to_element(selenium_element).perform()
+            self.logger.info(
+                f"Successfuly scrolled to desired element.",
+            )
+        except Exception as e:
+            self.logger.error(
+                f"(scroll_to_element) Some other exception: {e}",
+            )
