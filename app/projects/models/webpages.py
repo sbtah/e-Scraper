12d9eac76ac9f1ff056b2332b866611eb1a9845b
Sbtah <shisastore@protonmail.com>
2022-12-05 09:39:26 +0100

    Modified WebPage object.

--- app/projects/models/webpages.py
+++ app/projects/models/webpages.py
@@ -4,15 +4,16 @@ from projects.models.websites import Website
 
 class WebPage(models.Model):
     """
-    Base abstract class of all other types of Pages on site.
+    Base abstract class for all other types of Pages on site.
     """
 
     discovery_url = models.CharField(max_length=255, unique=True)
     current_url = models.CharField(max_length=255, blank=True)
-    meta_author = models.CharField(max_length=255, blank=True)
+
+    seo_title = models.CharField(max_length=255, blank=True)
     meta_description = models.TextField(blank=True)
     canonical_url = models.CharField(max_length=255, blank=True)
-    seo_title = models.CharField(max_length=255, blank=True)
+
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     parrent_website = models.ForeignKey(Website, on_delete=models.CASCADE)
