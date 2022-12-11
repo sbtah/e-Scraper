ca786ce1fc51c97e1e875a42a83488ac269a88e0
Sbtah <shisastore@protonmail.com>
2022-12-11 21:02:23 +0100

    Added 2 test cases for Website object.

--- app/tests/test_website.py
+++ app/tests/test_website.py
@@ -38,3 +38,52 @@ class TestWebsiteModel:
         assert website.scraper_class == creation_data["scraper_class"]
         assert isinstance(website.scraper_class, str)
         assert type(website.scraper_class) == type(creation_data["scraper_class"])
+
+    @pytest.mark.django_db(transaction=True)
+    def test_domain_module_name_scraper_class_must_be_unique(
+        self,
+        example_website,
+        website_create_test_data,
+    ):
+        """
+        Test that Website won't be created,
+            if there is already a Website object with same value for:
+            - domain,
+            - module_name,
+            - scraper_class.
+        """
+
+        creation_data = website_create_test_data
+        website_1 = example_website
+
+        with pytest.raises(Exception):
+            Website.objects.create(
+                domain=creation_data["domain"],
+                is_monitored=True,
+                module_name="teststore1",
+                scraper_class="TestStore1",
+            )
+
+        with pytest.raises(Exception):
+            Website.objects.create(
+                domain="test2.com",
+                is_monitored=True,
+                module_name=creation_data["module_name"],
+                scraper_class="TestStore2",
+            )
+
+        with pytest.raises(Exception):
+            Website.objects.create(
+                domain="test3.com",
+                is_monitored=True,
+                module_name="teststore3",
+                scraper_class=creation_data["scraper_class"],
+            )
+
+        assert Website.objects.all().count() == 1
+
+    def test_str_method_returns_proper_data(sel, example_website):
+        """Test that __str__ for object is properly returning data."""
+
+        website = example_website
+        assert str(website) == website.domain
