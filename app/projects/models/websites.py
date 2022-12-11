ce138ea9d86be1effdf7598cd325cfb8b04e0619
Sbtah <shisastore@protonmail.com>
2022-12-11 14:39:51 +0100

    Added is_monitored field to Website object.

--- app/projects/models/websites.py
+++ app/projects/models/websites.py
@@ -5,6 +5,7 @@ class Website(models.Model):
     """Base class for Website object."""
 
     domain = models.CharField(max_length=200, unique=True)
+    is_monitored = models.BooleanField(default=False)
     # Name of file for related Scraper.
     module_name = models.CharField(max_length=100)
     # Name of class for related Scraper.
